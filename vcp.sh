28018259acf545f115eba2308bee3f10e6aeaf45
Alex Frederiksen <ajfred154@gmail.com>
2018-03-14 16:13:16 -0400

    Initial commit

---
+++ vcp.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+FILE_C=10;
+ACTION_C=7;
+
+if [ $1 == "--help" ]; then
+    echo "Copies the current directory to the given one with intense logging.";
+    echo "";
+    echo "USAGE: vcp <destination>";
+    echo "";
+    exit;
+fi
+
+src=".";
+dst=$1;
+
+tput setaf $ACTION_C;
+echo "Preparing to copy:";
+
+count=0;
+tput setaf $FILE_C;
+while read i; do
+    echo "   $i";
+    count=$((count+1));
+done <<< $(find $src -type f)
+echo "";
+tput setaf $ACTION_C;
+echo "   Total: $count files";
+echo "";
+
+echo "Is this ok? [y/N]: ";
+read answer;
+if [ "$answer" != "y" ] && [ "$answer" != "Y" ]; then
+    echo "Aborting...";
+    tput sgr0;
+    exit;
+fi
+
+echo "";
+
+i=0;
+while read name; do
+    i=$((i+1));
+    cols=$(tput cols);
+    body_width=$(( $cols-24-4 ));
+    progress=$(( (100*$i)/$count ))
+    printf "   $(tput setaf $ACTION_C)Copying (%4d/%4d): $(tput setaf $FILE_C)%-*s$(tput setaf $ACTION_C)%%%3s\n" $i $count $body_width "$name..." "$progress";
+    cp --parent "$name" "$dst";
+done <<< $(find $src -type f)
+
+echo "";
+echo "Done."; 
+
+tput sgr0;
+
+
+
+
+
+
+
+
